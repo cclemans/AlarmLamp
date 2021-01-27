97f445a314ea5c440ad29e1cff2cab3d3ad49375
conrad <conrad.clemans@gmail.com>
2021-01-26 23:10:14 -0800

    test

---
+++ AlarmLamp/AlarmLamp.ino
@@ -0,0 +1,16 @@
+/*
+  Lamp Alarm
+  
+  Conrad Clemans
+  Portland, OR
+  01/26/20
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
